6b8635157bb21e9cdbd393c896da16ec3715bf47
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-07-02 14:33:13 -0500

    partner switch on map

--- client/components/Map.js
+++ client/components/Map.js
@@ -3,20 +3,32 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import stations from '../../script/data/stations.json';
 
 function Map() {
-  // const filteredStations = stations.filter(
-  //   (station) => station['Complex ID'] === 610 || station['Complex ID'] === 611
-  // );
+  const filteredStations = stations.filter(
+    (station) => station['Complex ID'] === 610 || station['Complex ID'] === 611
+  );
 
   return (
-    <MapContainer center={[40.7, -74]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[40.7, -74]} zoom={8} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      {/* {filteredStations.map((station) => (
-        <Marker>
-
-      ))} */}
+      {filteredStations.map((station) => (
+        <Marker
+          key={station['Station ID']}
+          position={[station['GTFS Latitude'], station['GTFS Longitude']]}
+        >
+          <Popup
+            position={[station['GTFS Latitude'], station['GTFS Longitude']]}
+          >
+            {' '}
+            <div>
+              <p>{station['Stop Name']}</p>
+              <p>{station.Line}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
