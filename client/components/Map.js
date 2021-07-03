e6df07bf0fea033e2ccd9e83b96580c401381db1
gytterberg <gytterberg@gmail.com>
2021-07-03 09:48:01 -0400

    event handlers: click, mouseover, mouseout

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MapContainer,
   TileLayer,
@@ -20,23 +20,68 @@ function Map() {
     station['GTFS Longitude'],
   ]);
 
+  const [selectedStation, setSelectedStation] = useState({
+    'Stop Name': 'No station selected',
+  });
+
   console.log(filteredStations);
   return (
-    <MapContainer
-      center={[40.785091, -73.968285]}
-      zoom={14}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e'
-        url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=481HF56KCNL52f9yp3TR"
-      />
-      {filteredStations.map((station) => (
+    <>
+      <p>Your selected station: {selectedStation['Stop Name']}</p>
+      <MapContainer
+        center={[40.785091, -73.968285]}
+        zoom={14}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e'
+          url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=481HF56KCNL52f9yp3TR"
+        />
+        {filteredStations.map((station) => (
+          <Marker
+            key={station['Station ID']}
+            position={[station['GTFS Latitude'], station['GTFS Longitude']]}
+            eventHandlers={{
+              mouseover: () => {
+                setSelectedStation(station);
+              },
+
+              mouseout: () => {
+                setSelectedStation({
+                  'Stop Name': 'No station selected',
+                });
+              },
+            }}
+          >
+            <Popup
+              position={[station['GTFS Latitude'], station['GTFS Longitude']]}
+            >
+              {' '}
+              <div>
+                <p>{station['Stop Name']}</p>
+                <p>
+                  {' '}
+                  Line: {station['Daytime Routes']} ({station.Line})
+                </p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+        <Polyline
+          pathOptions={{ color: '#808183', weight: 8 }}
+          positions={filteredLatLong}
+        />
+
         <Marker
-          key={station['Station ID']}
-          position={[station['GTFS Latitude'], station['GTFS Longitude']]}
+          position={[40.785091, -73.968285]}
+          title="A title"
+          eventHandlers={{
+            click: (e) => {
+              console.log('marker clicked', e);
+            },
+          }}
         >
-          <Popup
+          {/* <Popup
             position={[station['GTFS Latitude'], station['GTFS Longitude']]}
           >
             {' '}
@@ -47,14 +92,10 @@ function Map() {
                 Line: {station['Daytime Routes']} ({station.Line})
               </p>
             </div>
-          </Popup>
+          </Popup> */}
         </Marker>
-      ))}
-      <Polyline
-        pathOptions={{ color: '#808183', weight: 8 }}
-        positions={filteredLatLong}
-      />
-    </MapContainer>
+      </MapContainer>
+    </>
   );
 }
 
