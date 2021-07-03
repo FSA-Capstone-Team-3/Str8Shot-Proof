7342b44e73a49030e3387fa73f3e3f6c790f8449
alexhshapiro <alexhshapiro@gmail.com>
2021-07-03 16:33:42 -0400

    map rendering with all subway stops marked with default marker

--- client/components/Map.js
+++ client/components/Map.js
@@ -9,49 +9,16 @@ import {
 } from "react-leaflet";
 //import stations from "../../script/data/stations.json";
 import allLines from "../../script/data/subway_lines.geojson";
+import allStops from "../../script/data/subway_stops.geojson";
 
 function Map() {
-  // const filteredStations = stations.filter(
-  //   (station) =>
-  //     (station["Complex ID"] === 610 || station["Complex ID"] === 611) &&
-  //     station["Daytime Routes"] === "S"
-  // );
 
-  // const filteredLatLong = filteredStations.map((station) => [
-  //   station["GTFS Latitude"],
-  //   station["GTFS Longitude"],
-  // ]);
-
-  // console.log(filteredStations);
   return (
     <MapContainer center={[40.7, -74]} zoom={8} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* {filteredStations.map((station) => (
-        <Marker
-          key={station["Station ID"]}
-          position={[station["GTFS Latitude"], station["GTFS Longitude"]]}
-        >
-          <Popup
-            position={[station["GTFS Latitude"], station["GTFS Longitude"]]}
-          >
-            {" "}
-            <div>
-              <p>{station["Stop Name"]}</p>
-              <p>
-                {" "}
-                Line: {station["Daytime Routes"]} ({station.Line})
-              </p>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-      <Polyline
-        pathOptions={{ color: "#808183", weight: 8 }}
-        positions={filteredLatLong}
-      /> */}
 
       <GeoJSON
         data={allLines}
@@ -106,6 +73,21 @@ function Map() {
           }
         }}
       />
+      <GeoJSON data={allStops} style={function(feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: "#ffffff",
+            radius: 7,
+            pane: 'ALL'
+          }).bindPopup(feature.properties.stop_name)
+          .openPopup().on('click', nada_stops)
+          .on('mouseover', function(e) {
+            this.openPopup();
+          })
+          .on('mouseout', function(e) {
+            this.closePopup();
+          });
+    }}/>
+
     </MapContainer>
   );
 }
