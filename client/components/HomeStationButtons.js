2a2df6504d8ce626eadad13550683460eb4f5149
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-07-09 10:00:38 -0500

    cleaned up clutter

--- client/components/HomeStationButtons.js
+++ client/components/HomeStationButtons.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { postStation, deleteStation } from '../store/stations';
-import { homeStationsCheck } from '../utils/homeStationsCheck';
 
 function HomeStationButtons({ selectedStation, homeStations }) {
   const dispatch = useDispatch();
+
+  const homeStationsCheck = (homeStations, selectedStation) => {
+    for (let i = 0; i < homeStations.length; i++) {
+      let currentStation = homeStations[i];
+
+      if (currentStation.code === selectedStation.properties['stop_id']) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   return (
     <React.Fragment>
       {homeStationsCheck(homeStations, selectedStation) ? (
