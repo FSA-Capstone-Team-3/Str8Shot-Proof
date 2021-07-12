e4bc3fa40bdced7fa42c8f4491ebb6abf3716fb1
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-07-12 00:48:46 -0500

    started working on a loader. isnt finished and will make changes if need be

--- client/App.js
+++ client/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 import MyStations from './components/MyStations';
-
+import Loader from './components/Loader';
 const App = () => {
   return (
     <div>
+      {/* <Loader /> */}
       <Navbar />
       <Routes />
     </div>
---
+++ client/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Loader = () => {
+  return (
+    <div id='loader-wrapper'>
+      <div id='loader'>
+        {/* <img src='8logo.png' alt='site logo' style={{ maxHeight: '100px' }} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
